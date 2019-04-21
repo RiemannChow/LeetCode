8d05372ff07e6b66cdec687b2bb819f7f05536a2
riemannChow <754798352@qq.com>
2019-04-21 22:09:47 +0800

    Request

---
+++ src/main/java/com/test/filter/Request.java
@@ -0,0 +1,21 @@
+package com.test.filter;
+
+/**
+ * @author riemann
+ * @date 2019/04/14 21:16
+ *
+ * 封装请求的类Request
+ */
+public class Request {
+
+    String requestStr;
+
+    public String getRequestStr() {
+        return requestStr;
+    }
+
+    public void setRequestStr(String requestStr) {
+        this.requestStr = requestStr;
+    }
+
+}
