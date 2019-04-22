fa77ce223305c30fb0b1fcfd82be867f590b55ca
riemannChow <754798352@qq.com>
2019-04-22 23:23:23 +0800

    Response

---
+++ src/main/java/com/test/filter/Response.java
@@ -0,0 +1,22 @@
+package com.test.filter;
+
+/**
+ * @author riemann
+ * @date 2019/04/14 21:18
+ *
+ * 封装响应信息的类Response
+ */
+public class Response {
+
+    String responseStr;
+
+    public String getResponseStr() {
+        return responseStr;
+    }
+
+    public void setResponseStr(String responseStr) {
+        this.responseStr = responseStr;
+    }
+
+}
+
