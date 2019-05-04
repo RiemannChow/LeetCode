389fe0fb13cd81eac271b16dc2f7c083685bcf5d
riemann <754798352@qq.com>
2019-05-04 22:59:57 +0800

    TestMethod

---
+++ src/main/java/com/scoket/io/BreakpointUpLoad/TestMethod.java
@@ -0,0 +1,29 @@
+package com.scoket.io.BreakpointUpLoad;
+
+/**
+ * @author riemann
+ * @date 2019/04/24 23:26
+ */
+public class TestMethod {
+    public TestMethod()
+    {
+        try
+        {
+            SiteInfoBean bean = new SiteInfoBean(
+                    "http://banzou.cdn.aliyun.com/apk/changba_6093.apk",
+                    "E:\\Test", "changba_6093.apk", 3);
+
+            SiteFileFetch fileFetch = new SiteFileFetch(bean);
+            fileFetch.start();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new TestMethod();
+    }
+}
