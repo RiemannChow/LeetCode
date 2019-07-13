4e2bc004c33a9cf2c59be4cae65655da2e750d81
riemann <754798352@qq.com>
2019-07-14 02:44:43 +0800

    Add custom circular single linked list for thread safety

---
+++ src/main/java/com/algorithm/list/CircularLinkedListTest.java
@@ -0,0 +1,29 @@
+package com.algorithm.list;
+
+import org.junit.Test;
+
+/**
+ * @author riemann
+ * @date 2019/07/14 1:24
+ */
+public class CircularLinkedListTest {
+
+    @Test
+    public void test() {
+        CircularLinkedList<String> list = new ConcurrentCircularLinkedList<String>();
+        for (int i = 1; i < 101; i++) {
+            list.add("" + i);
+        }
+        int count = 1;
+        while (list.size() > 1) {
+            list.next();
+            count++;
+            if (count % 3 == 0) {
+                System.out.println(list.remove() + " break!");
+                count++;
+            }
+        }
+        System.out.println(list.toString());
+    }
+
+}
