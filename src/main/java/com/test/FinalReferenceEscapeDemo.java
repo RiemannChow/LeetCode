39c5c4c90761d2d38c45127c766d5a4cef8a5b4f
riemann <754798352@qq.com>
2019-08-25 19:54:52 +0800

    FinalReferenceEscapeDemo

---
+++ src/main/java/com/test/FinalReferenceEscapeDemo.java
@@ -0,0 +1,27 @@
+package com.test;
+
+/**
+ * @author riemann
+ * @date 2019/07/18 0:46
+ */
+public class FinalReferenceEscapeDemo {
+
+    private final int a;
+    private FinalReferenceEscapeDemo referenceDemo;
+
+    public FinalReferenceEscapeDemo() {
+        a = 1;  //1
+        referenceDemo = this; //2
+    }
+
+    public void writer() {
+        new FinalReferenceEscapeDemo();
+    }
+
+    public void reader() {
+        if (referenceDemo != null) {  //3
+            int temp = referenceDemo.a; //4
+        }
+    }
+
+}
