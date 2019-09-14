6aaa87ce127b49f2008361ddcb59cb516f19a441
riemann <754798352@qq.com>
2019-09-14 15:47:40 +0800

    反射调用静态方法,不需要获取类对象

--- src/main/java/com/reflect/staticAndNoStatic/StaticAndNoStaticTest.java
+++ src/main/java/com/reflect/staticAndNoStatic/StaticAndNoStaticTest.java
@@ -10,6 +10,14 @@ import java.lang.reflect.Method;
 public class StaticAndNoStaticTest {
 
     public static void main(String[] args) throws Exception {
+      /*// 反射调用静态方法,不需要获取类对象
+        // 获取字节码对象
+        Class<StaticAndNoStaticTest> clazz = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
+        String[] s = new String[]{"riemann", "chow"};
+        // 获取Method对象
+        Method staticMethod = clazz.getMethod("staticMethod", String[].class);
+        // 调用invoke方法来调用
+        staticMethod.invoke(null, (Object) s);*/
 
         // 反射调用非静态方法，需要获取类对象
         // 获取字节码对象
