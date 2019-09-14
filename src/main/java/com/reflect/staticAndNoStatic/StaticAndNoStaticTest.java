a3c4425a1ca5bad8887f2c763aa88c8938d3db34
riemann <754798352@qq.com>
2019-09-14 16:12:42 +0800

    反射调用私有静态方法

--- src/main/java/com/reflect/staticAndNoStatic/StaticAndNoStaticTest.java
+++ src/main/java/com/reflect/staticAndNoStatic/StaticAndNoStaticTest.java
@@ -10,26 +10,38 @@ import java.lang.reflect.Method;
 public class StaticAndNoStaticTest {
 
     public static void main(String[] args) throws Exception {
-      /*// 反射调用静态方法,不需要获取类对象
+        System.out.println("==========反射调用静态方法==========");
+        // 反射调用静态方法,不需要获取类对象
         // 获取字节码对象
-        Class<StaticAndNoStaticTest> clazz = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
+        Class<StaticAndNoStaticTest> clazzStatic = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
         String[] s = new String[]{"riemann", "chow"};
         // 获取Method对象
-        Method staticMethod = clazz.getMethod("staticMethod", String[].class);
+        Method staticMethod = clazzStatic.getMethod("staticMethod", String[].class);
         // 调用invoke方法来调用
-        staticMethod.invoke(null, (Object) s);*/
+        staticMethod.invoke(null, (Object) s);
+
+        System.out.println("==========反射调用非静态方法==========");
 
         // 反射调用非静态方法，需要获取类对象
         // 获取字节码对象
-        Class<StaticAndNoStaticTest> clazz = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
+        Class<StaticAndNoStaticTest> clazzNoStatic = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
         // 获取一个对象
-        Constructor<StaticAndNoStaticTest> constructor = clazz.getConstructor();
+        Constructor<StaticAndNoStaticTest> constructor = clazzNoStatic.getConstructor();
         StaticAndNoStaticTest instance = constructor.newInstance();
-        String[] s = new String[]{"riemann", "chow"};
+        String[] ss = new String[]{"riemann", "chow"};
         // 获取Method对象
-        Method noStaticMethod = clazz.getMethod("NoStaticMethod", String[].class);
+        Method noStaticMethod = clazzNoStatic.getMethod("NoStaticMethod", String[].class);
         // 调用invoke方法来调用
-        noStaticMethod.invoke(instance, (Object) s);
+        noStaticMethod.invoke(instance, (Object) ss);
+
+        System.out.println("==========反射调用私有静态方法==========");
+        Class<StaticAndNoStaticTest> clazzStaticPrivate = (Class<StaticAndNoStaticTest>) Class.forName("com.reflect.staticAndNoStatic.StaticAndNoStaticTest");
+        Constructor<StaticAndNoStaticTest> constructor2 = clazzStaticPrivate.getConstructor();
+        StaticAndNoStaticTest instance2 = constructor2.newInstance();
+        Method staticPrivateMethod = clazzStaticPrivate.getDeclaredMethod("staticPrivateMethod", new Class[]{String.class});
+        staticPrivateMethod.setAccessible(true);
+        staticPrivateMethod.invoke(instance2, "test static and private");
+
     }
 
     public static void staticMethod(String[] args) {
@@ -42,6 +54,14 @@ public class StaticAndNoStaticTest {
         for (String str : args) {
             System.out.println(str);
         }
+    }
+
+    private static void staticPrivateMethod(String s) {
+        System.out.println("this is a private method and the parameters is: " + s);
+    }
+
+    private void noStaticPrivateMethod(String s) {
+        System.out.println("this is a private method and the parameters is: " + s);
     }
 
 }
