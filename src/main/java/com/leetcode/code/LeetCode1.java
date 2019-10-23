f00716be67e4ab4e5c15979b559775c091ed7172
riemann <754798352@qq.com>
2019-10-23 23:32:13 +0800

    LeetCode1

--- src/main/java/com/leetcode/code/LeetCode1.java
+++ src/main/java/com/leetcode/code/LeetCode1.java
@@ -12,7 +12,7 @@ import java.util.Map;
  */
 public class LeetCode1 {
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
