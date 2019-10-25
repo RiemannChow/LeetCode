d2a60b83789545b335075cc1b0f3653e8a9fed83
riemann <754798352@qq.com>
2019-10-26 00:17:17 +0800

    LeetCode3

---
+++ src/main/java/com/leetcode/code/LeetCode3.java
@@ -0,0 +1,40 @@
+package com.leetcode.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class LeetCode3 {
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int start = 0, end = 0, len = 0;
+        for (; start < n && end < n; end++) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(start, map.get(s.charAt(end)) + 1);
+            }
+            map.put(s.charAt(end), end);
+            len = Math.max(len, end - start + 1);
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LeetCode3.lengthOfLongestSubstring("pwwkew"));
+    }
+}
