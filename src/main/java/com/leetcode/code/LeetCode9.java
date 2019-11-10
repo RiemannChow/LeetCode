dad9e57cad45d02e59a4a1a3c13e0672d6e90004
riemann <754798352@qq.com>
2019-11-11 00:03:00 +0800

    LeetCode9

---
+++ src/main/java/com/leetcode/code/LeetCode9.java
@@ -0,0 +1,42 @@
+package com.leetcode.code;
+
+/**
+ * LeetCode9-回文数
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ * 输入: 121
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ * 示例 3:
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ * 进阶:
+ * 你能不将整数转为字符串来解决这个问题吗？
+ */
+public class LeetCode9 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        int revertedNum = 0;
+        int y = x;
+        while (x != 0) {
+            revertedNum = revertedNum * 10 + x % 10;
+            x = x / 10;
+        }
+        return revertedNum == y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(10));
+    }
+}
