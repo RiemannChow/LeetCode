4f390debe87c44b96debe0fb62f23076ee3e2342
riemann <754798352@qq.com>
2019-11-21 10:29:47 +0800

    InsertionSort

---
+++ src/main/java/com/algorithm/nineSortingAlgorithm/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.algorithm.nineSortingAlgorithm;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    private static void insertionSort(int[] arr) {
+        int preIndex, current;
+        for (int i = 1; i < arr.length; i++) {
+            preIndex = i - 1;
+            current = arr[i];
+            while (preIndex >= 0 && arr[preIndex] > current) {
+                arr[preIndex + 1] = arr[preIndex];
+                preIndex--;
+            }
+            arr[preIndex + 1] = current;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,28,3,21,11,7,6,18};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
