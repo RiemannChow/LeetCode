a69b661fd54bb986b358a875ebe76d407883b96d
riemann <754798352@qq.com>
2019-11-28 21:25:53 +0800

    LeetCode13

---
+++ src/main/java/com/leetcode/code/LeetCode13.java
@@ -0,0 +1,101 @@
+package com.leetcode.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * LeetCode13-罗马数字转整数
+ *
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ *
+ * 字符          数值
+ *  * I             1
+ *  * V             5
+ *  * X             10
+ *  * L             50
+ *  * C             100
+ *  * D             500
+ *  * M             1000
+ *
+ * 示例 1:
+ *
+ * 输入: "III"
+ * 输出: 3
+ *
+ * 示例 2:
+ *
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ *
+ * 示例 3:
+ *
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ */
+public class LeetCode13 {
+    // 方法一
+    public static int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int ans = 0;
+        for (int i = 0; i < s.length();) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                ans += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i + 1));
+                i += 1;
+            }
+        }
+        return ans;
+    }
+
+    // 方法二
+    public static int romanToInt2(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    private static int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt2("MCMXC"));
+    }
+}
