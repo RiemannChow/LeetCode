0af670002fe313719b03abcaa8734aa63895b7cd
riemann <754798352@qq.com>
2019-12-01 00:06:39 +0800

    LeetCode17

---
+++ src/main/java/com/leetcode/code/LeetCode17.java
@@ -0,0 +1,41 @@
+package com.leetcode.code;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * LeetCode17-电话号码的字母组合
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ * 示例:
+ *
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
+public class LeetCode17 {
+    public static List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if (digits.isEmpty()) {
+            return ans;
+        }
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int x = Character.getNumericValue(digits.charAt(i));
+            while (ans.peek().length() == i) {
+                String t = ans.remove();
+                for (char s : mapping[x].toCharArray()) {
+                    ans.add(t + s);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+    }
+}
