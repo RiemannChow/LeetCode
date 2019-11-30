7e519cc2d17d45727be9445b913ba7ceec1b4f98
riemann <754798352@qq.com>
2019-11-30 19:12:41 +0800

    LeetCode16

--- src/main/java/com/leetcode/code/LeetCode16.java
+++ src/main/java/com/leetcode/code/LeetCode16.java
@@ -3,6 +3,8 @@ package com.leetcode.code;
 import java.util.Arrays;
 
 /**
+ * LeetCode16-最接近的三数之和
+ *
  * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
  * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
  * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
@@ -10,34 +12,29 @@ import java.util.Arrays;
 
 public class LeetCode16 {
     public static int threeSumClosest(int[] nums, int target) {
-        if(nums.length < 3){
-           return 0;
+        if (nums.length < 3) {
+            return 0;
         }
         Arrays.sort(nums);
-        int result = 0;
-        int cha = Integer.MAX_VALUE;
-        int tempCha = 0;
-        for (int one = 0; one < nums.length - 2; one++) {
-            int left = one + 1;
-            int right = nums.length - 1;
-            while(left < right){
-                int tempSum = nums[one] + nums[left] + nums[right];
-                tempCha = Math.abs(tempSum - target);
-                if(tempCha < cha){
-                    cha = tempCha;
-                    result = tempSum;//这里的潜在的最后的正确结果没注意保存
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
                 }
-                if(tempSum == target){
-                    return target;
-                }
-                if(tempSum < target){
-                    left++;
-                }else{
+                if (sum > target) {
                     right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    return ans;
                 }
+
             }
         }
-        return result;
+        return ans;
     }
 
     public static void main(String[] args) {
