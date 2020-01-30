63aec1542b2590ba96f7d7d818f0ba3a2a0e7710
riemann <754798352@qq.com>
2020-01-30 15:04:16 +0800

    ConditionalCompilation

---
+++ src/main/java/com/syntacticSugar/ConditionalCompilation.java
@@ -0,0 +1,16 @@
+package com.syntacticSugar;
+
+public class ConditionalCompilation {
+    public static void main(String[] args) {
+        final boolean DEBUG = true;
+        if(DEBUG) {
+            System.out.println("Hello, DEBUG!");
+        }
+
+        final boolean ONLINE = false;
+
+        if(ONLINE){
+            System.out.println("Hello, ONLINE!");
+        }
+    }
+}
