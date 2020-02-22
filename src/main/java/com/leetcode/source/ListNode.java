54dd06e7963261b2a3150bbdceb687dc541c8373
riemann <754798352@qq.com>
2020-02-22 19:47:28 +0800

    ListNode

--- src/main/java/com/leetcode/source/ListNode.java
+++ src/main/java/com/leetcode/source/ListNode.java
@@ -1,11 +1,27 @@
 package com.leetcode.source;
 
 public class ListNode {
-    int val;
+    public int data;
 
     public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode getNext() {
+        return this.next;
     }
 }
