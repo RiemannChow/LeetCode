978c1688847284f507a1025403f98887b85c4d8a
riemann <754798352@qq.com>
2020-03-03 16:11:14 +0800

    Node

---
+++ src/main/java/com/algorithm/list/doublyLinkedList/Node.java
@@ -0,0 +1,41 @@
+package com.algorithm.list.doublyLinkedList;
+
+public class Node {
+    private int id;
+
+    private String name;
+
+    private Node prev, next;// 前驱与后驱结点
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
