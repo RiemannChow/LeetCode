12a33e2c9f7d046955c2cef9724932f038cf6042
riemann <754798352@qq.com>
2020-03-05 21:54:33 +0800

    ArrayQueue

---
+++ src/main/java/com/algorithm/queue/ArrayQueue.java
@@ -0,0 +1,46 @@
+package com.algorithm.queue;
+
+/**
+ * 基于数组实现的队列
+ */
+public class ArrayQueue {
+    // 数组：items，数组大小：n
+    private String[] items;
+    private int n = 0;
+    // head表示队头下标，tail表示队尾下标
+    private int head = 0;
+    private int tail = 0;
+
+    // 申请一个大小为capacity的数组
+    public ArrayQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    // 入队操作，将item放入队尾
+    public boolean enqueue(String item) {
+        // 如果tail == n 表示队列已经满了
+        if (tail == n) {
+            // tail ==n && head==0，表示整个队列都占满了
+            if (head == 0) return false;
+            for (int i = head; i < tail; i++) {
+                items[i - head] = items[i];
+            }
+            // 搬移完之后重新更新head和tail
+            tail -= head;
+            head = 0;
+        }
+        items[tail] = item;
+        ++tail;
+        return true;
+    }
+
+    // 出队
+    public String dequeue() {
+        // 如果head == tail 表示队列为空
+        if (head == tail) return null;
+        String temp = items[head];
+        ++head;
+        return temp;
+    }
+}
