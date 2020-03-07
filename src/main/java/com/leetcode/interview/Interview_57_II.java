1b405c30e64510418accf4b8cd20d9588c4bbb5c
riemann <754798352@qq.com>
2020-03-07 14:25:53 +0800

    Interview_57_II

---
+++ src/main/java/com/leetcode/interview/Interview_57_II.java
@@ -0,0 +1,47 @@
+package com.leetcode.interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Interview_57_II-和为s的连续正数序列
+ *
+ * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+ *
+ * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+ *
+ * 示例：
+ * 输入：target = 15
+ * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+ */
+public class Interview_57_II {
+    public int[][] findContinuousSequence(int target) {
+        int i = 1; // 滑动窗口的左边界
+        int j = 1; // 滑动窗口的右边界
+        int sum = 0; // 滑动窗口中数字的和
+        List<int[]> res = new ArrayList<>();
+
+        while (i <= target / 2) {
+            if (sum < target) {
+                // 右边界向右移动
+                sum += j;
+                j++;
+            } else if (sum > target) {
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            } else {
+                // 记录结果
+                int[] arr = new int[j - i];
+                for (int k = i; k < j; k++) {
+                    arr[k - i] = k;
+                }
+                res.add(arr);
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
