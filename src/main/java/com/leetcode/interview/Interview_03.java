fd697871817798b4c0726e10669d8cb0719406f0
riemann <754798352@qq.com>
2020-03-07 14:24:07 +0800

    Interview_03

---
+++ src/main/java/com/leetcode/interview/Interview_03.java
@@ -0,0 +1,39 @@
+package com.leetcode.interview;
+
+/**
+ * Interview_03-数组中重复的数字
+ *
+ * 找出数组中重复的数字。
+ *
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ *
+ * 示例：
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ */
+public class Interview_03 {
+    // 方法1
+    public int findRepeatNumber(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]]++;
+            if (arr[nums[i]] > 1) return nums[i];
+        }
+        return -1;
+    }
+
+    // 方法2
+    public int findRepeatNumber2(int[] nums) {
+        for(int i = 0; i < nums.length; ++i) {
+            while(nums[i] != i){
+                if(nums[i] == nums[nums[i]])
+                    return nums[i];
+                int temp = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = temp;
+            }
+        }
+        return -1;
+    }
+}
