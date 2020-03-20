24382a044a834e89e6185914371f953f386cc638
riemann <754798352@qq.com>
2020-03-21 00:25:33 +0800

    BinarySearch

--- src/main/java/com/algorithm/binarySearch/BinarySearch.java
+++ src/main/java/com/algorithm/binarySearch/BinarySearch.java
@@ -145,50 +145,50 @@ public class BinarySearch {
     }
 
     // 在旋转排序数组中搜索 (不考虑重复项)
-    public static int binarySearch_7(int[] arr, int n, int target) {
-        int low = 0, high = n - 1;
+    public static int binarySearch_7(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[low] <= arr[mid]) {
-                if (target < arr[mid] && target >= arr[low]) {
-                    low = mid - 1;
-                } else {
-                    high = mid + 1;
-                }
-            } else if (arr[mid] <= arr[high]) {
-                if (target > arr[mid] && target <= arr[high]) {
+            if (arr[mid] == target) return mid;
+            if (arr[low] <= arr[mid]) { // 左边升序,注意此处用小于等于
+                if (target >= arr[low] && target < arr[mid]) { // 在左边范围内
+                    high = mid - 1;
+                } else { // 只能从右边找
                     low = mid + 1;
-                } else {
+                }
+            } else { // 右边升序
+                if (target <= arr[high] && target > arr[mid]) { // 在右边范围内
+                    low = mid + 1;
+                } else { // 只能从左边找
                     high = mid - 1;
                 }
             }
         }
-        return arr[low];
+        return -1;
     }
 
     // 在旋转排序数组中搜索 (存在重复项)
-    public static boolean binarySearch_8(int[] arr, int n, int target) {
-        int low = 0, high = n - 1;
+    public static boolean binarySearch_8(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if (arr[mid] == target) {
-                return true;
-            } else if (arr[high] < arr[mid]) {
-                if (target < arr[mid] && target >= arr[low]) {
-                   high = mid;
-                } else {
+            if (arr[mid] == target) return true;
+            if (arr[low] == arr[mid]) {
+                low++;
+                continue;
+            }
+            if (arr[low] <= arr[mid]) { // 左边升序,注意此处用小于等于
+                if (target >= arr[low] && target < arr[mid]) { // 在左边范围内
+                    high = mid - 1;
+                } else { // 只能从右边找
                     low = mid + 1;
                 }
-            } else if (arr[mid] < arr[high]) {
-                if (target > arr[mid] && target <= arr[high]) {
+            } else { // 右边升序
+                if (target <= arr[high] && target > arr[mid]) { // 在右边范围内
                     low = mid + 1;
-                } else {
-                    high = mid;
+                } else { // 只能从左边找
+                    high = mid - 1;
                 }
-            } else {
-                high--;
             }
         }
         return false;
