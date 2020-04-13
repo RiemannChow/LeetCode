78ec66789147f6ae9e29710d7dc290c8ff239b04
riemann <754798352@qq.com>
2020-04-13 23:56:38 +0800

    LeetCode104

--- src/main/java/com/leetcode/code/LeetCode104.java
+++ src/main/java/com/leetcode/code/LeetCode104.java
@@ -5,21 +5,19 @@ package com.leetcode.code;
  * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
  *
  * 说明: 叶子节点是指没有子节点的节点。
- *   3
+ *     3
  *    / \
  *   9  20
  *     /  \
  *    15   7
- *    返回它的最大深度 3 。
+ * 返回它的最大深度 3 。
  */
 
 import com.leetcode.source.TreeNode;
 
 public class LeetCode104 {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
+        if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
