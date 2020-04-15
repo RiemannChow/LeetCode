f7677e036be40d23fdddf8c3a8612c7c2181e36a
riemann <754798352@qq.com>
2020-04-15 21:46:24 +0800

    LeetCode450

---
+++ src/main/java/com/leetcode/code/LeetCode450.java
@@ -0,0 +1,75 @@
+package com.leetcode.code;
+
+import com.leetcode.source.TreeNode;
+
+/**
+ * LeetCode450-删除二叉搜索树中的节点
+ *
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。
+ * 返回二叉搜索树（有可能被更新）的根节点的引用。
+ *
+ * 一般来说，删除节点可分为两个步骤：
+ * 1、首先找到需要删除的节点；
+ * 2、如果找到了，删除它。
+ *
+ * 示例：
+ *
+ * root = [5,3,6,2,4,null,7]
+ * key = 3
+ *
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * 给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ *
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ *
+ *     5
+ *    / \
+ *   4   6
+ *  /     \
+ * 2       7
+ *
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ *
+ *     5
+ *    / \
+ *   2   6
+ *    \   \
+ *     4   7
+ *
+ */
+
+public class LeetCode450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (key > root.val) root.right = deleteNode(root.right, key);
+        else if (key < root.val) root.left = deleteNode(root.left, key);
+        else {
+            if (root.left == null && root.right == null) root = null;
+            else if (root.right != null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            } else if (root.left != null) {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+
+    private int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+
+    private int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+}
